2039f45c458cdb9ba4bc13e306ce0af45d222fc8
Daniel Montoya <dacamoty@gmail.com>
2025-06-03 10:45:33 -0500

    Se agrega rutas para rn

---
+++ app/_layout.jsx
@@ -0,0 +1,14 @@
+import { Slot } from "expo-router";
+import { View } from "react-native";
+import "../global.css";
+
+export default function Layout() {
+  return (
+    <View
+      style={{ flex: 1, backgroundColor: "#000" }}
+      className="items-center justify-center"
+    >
+      <Slot />
+    </View>
+  );
+}
